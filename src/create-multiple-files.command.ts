abc442ba28f82b2e9b134e89ec503597afab779b
igorlau <igorassislau@gmail.com>
2024-10-26 18:35:33 -0300

    feat: implement multi-step input form

--- src/create-multiple-files.command.ts
+++ src/create-multiple-files.command.ts
@@ -1,5 +1,8 @@
-import type { QuickPickItem, WorkspaceFolder } from "vscode";
-import { FileType, Uri, window, workspace } from "vscode";
+import type {
+  QuickPickItem,
+  WorkspaceFolder as VSWorkspaceFolder,
+} from "vscode";
+import { FileType, Uri, workspace } from "vscode";
 import { MultiStepForm } from "./utils/multi-step-input-form";
 
 export type TSettingsTemplate = {
@@ -12,7 +15,7 @@ export type TCreationTemplate = QuickPickItem & {
   suffixes: string[];
 };
 
-export type TWorkspaceFolder = QuickPickItem & WorkspaceFolder;
+export type TWorkspaceFolder = QuickPickItem & VSWorkspaceFolder;
 
 type CreateMultipleFilesState = {
   workspace: TWorkspaceFolder | null;
@@ -36,106 +39,116 @@ export class CreateMultipleFilesCommand extends MultiStepForm<CreateMultipleFile
       prefix: null,
     };
 
-    super(5, initialState);
+    super(initialState);
   }
 
   async execute() {
-    const workspace = await this.getWorkspaceFolderPrompt();
-
-    if (!workspace) {
-      return;
-    }
-
-    const selectedTemplate = await this.getTemplatePrompt(workspace);
-
-    if (!selectedTemplate) {
-      return;
-    }
-
-    const hasCustomTemplateSelected =
-      selectedTemplate.label.toLowerCase() ===
-      this.CUSTOM_SETTINGS_TEMPLATE.label.toLowerCase();
-
-    if (hasCustomTemplateSelected) {
-      const customSuffixes = await this.getSuffixesPrompt();
-      selectedTemplate.suffixes = customSuffixes;
-    }
-
-    if (selectedTemplate.suffixes.length === 0) {
-      window.showInformationMessage("No suffixes provided.");
-      return;
-    }
-
-    const folder = await this.getDestinationFolderPrompt(workspace);
-
-    if (!folder) {
-      return;
-    }
-
-    const prefix = await this.getFilesPrefixPrompt();
-
-    if (!prefix) {
-      return;
-    }
-
-    await this.createFoldersAndFilesTask(
-      folder,
-      selectedTemplate.suffixes,
-      prefix
-    );
-  }
-
-  private async getWorkspaceFolderPrompt(): Promise<
-    TWorkspaceFolder | undefined
-  > {
     const workspaces = workspace.workspaceFolders;
 
     if (!workspaces || workspaces.length === 0) {
       return undefined;
     }
 
-    if (workspaces.length === 1) {
-      return {
-        ...workspaces[0],
-        label: workspaces[0].name,
-        detail: workspace.asRelativePath(workspaces[0].uri.path),
-      };
+    await this.stepThrough({
+      1: {
+        shouldSkip: () => workspaces.length === 1,
+        whenSkip: () => {
+          this.updateState("workspace", {
+            ...workspaces[0],
+            label: workspaces[0].name,
+            detail: workspace.asRelativePath(workspaces[0].uri.path),
+          });
+        },
+        execute: async () => {
+          const selectedWorkspace = await this.getWorkspaceFolderPrompt(
+            workspaces
+          );
+          this.updateState("workspace", selectedWorkspace ?? null);
+        },
+      },
+      2: {
+        shouldSkip: () => false,
+        execute: async () => {
+          const selectedTemplate = await this.getTemplatePrompt();
+          this.updateState("template", selectedTemplate ?? null);
+        },
+      },
+      3: {
+        shouldSkip: (currentState) =>
+          currentState.template?.label.toLowerCase() !==
+          this.CUSTOM_SETTINGS_TEMPLATE.label.toLowerCase(),
+        execute: async () => {
+          const selectedSuffixes = await this.getSuffixesPrompt();
+
+          const currentTemplate = {
+            ...this.state.template,
+            suffixes: selectedSuffixes,
+          } as TCreationTemplate;
+
+          this.updateState("template", currentTemplate);
+        },
+      },
+      4: {
+        shouldSkip: () => false,
+        execute: async () => {
+          const selectedFolder = await this.getDestinationFolderPrompt();
+          this.updateState(
+            "folder",
+            selectedFolder
+              ? { uri: selectedFolder.uri, relativePath: selectedFolder.path }
+              : null
+          );
+        },
+      },
+      5: {
+        shouldSkip: () => false,
+        execute: async () => {
+          const selectedPrefix = await this.getFilesPrefixPrompt();
+          this.updateState("prefix", selectedPrefix ?? null);
+        },
+      },
+    });
+
+    if (!this.state.folder || !this.state.prefix || !this.state.template) {
+      return;
     }
 
+    const destinationFolderUri = Uri.joinPath(this.state.folder.uri);
+
+    await this.createFolderStructure(destinationFolderUri);
+
+    for (const suffix of this.state.template.suffixes) {
+      const fileName = `${this.state.prefix}${suffix}`;
+      const fileUri = Uri.joinPath(destinationFolderUri, fileName);
+
+      await workspace.fs.writeFile(fileUri, new Uint8Array());
+    }
+  }
+
+  private async getWorkspaceFolderPrompt(
+    workspaces: readonly VSWorkspaceFolder[]
+  ): Promise<TWorkspaceFolder | undefined> {
     const items = workspaces.map<TWorkspaceFolder>((item) => ({
       ...item,
       label: item.name,
       detail: workspace.asRelativePath(item.uri.path),
     }));
 
-    const selectedWorkspace = await window.showQuickPick(items, {
+    const selectedWorkspace = await this.showSingleSelectionQuickPick({
+      activeItem: this.state.workspace ?? undefined,
+      canPickMany: false,
+      ignoreFocusOut: true,
+      items,
+      placeholder: "Select the workspace in which the files will be created",
       title: "Workspace selection",
-      placeHolder: "Select the workspace in which the files will be created",
-      ignoreFocusOut: true,
     });
 
-    return selectedWorkspace;
+    return selectedWorkspace as TWorkspaceFolder | undefined;
   }
 
-  private async getTemplatePrompt(
-    workspaceFolder: TWorkspaceFolder
-  ): Promise<TCreationTemplate | undefined> {
-    const templates = this.loadTemplates(workspaceFolder);
-
-    const selectedTemplate = await window.showQuickPick(templates, {
-      title: "Template selection",
-      placeHolder: "Select a predefined template or use a custom creation",
-      ignoreFocusOut: true,
-    });
-
-    return selectedTemplate;
-  }
-
-  private async loadTemplates(
-    workspaceFolder: TWorkspaceFolder
-  ): Promise<TCreationTemplate[]> {
+  private async getTemplatePrompt(): Promise<TCreationTemplate | undefined> {
     const templates = workspace
-      .getConfiguration("create-files-batch", workspaceFolder.uri)
+      .getConfiguration("create-files-batch", this.state.workspace?.uri)
       .get<TSettingsTemplate[]>("templates", []);
 
     templates.push(this.CUSTOM_SETTINGS_TEMPLATE);
@@ -147,16 +160,25 @@ export class CreateMultipleFilesCommand extends MultiStepForm<CreateMultipleFile
       suffixes: template.suffixes,
     }));
 
-    return creationTemplates;
+    const selectedTemplate = await this.showSingleSelectionQuickPick({
+      activeItem: this.state.template ?? undefined,
+      ignoreFocusOut: true,
+      items: creationTemplates,
+      placeholder: "Select a predefined template or use a custom creation",
+      title: "Template selection",
+    });
+
+    return selectedTemplate as TCreationTemplate | undefined;
   }
 
   private async getSuffixesPrompt(): Promise<string[]> {
-    const suffixesInput = await window.showInputBox({
-      title: "Files custom suffixes",
+    const suffixesInput = await this.showInputBox({
+      ignoreFocusOut: true,
       placeHolder: "Enter comma separated values...",
       prompt:
         "Each item must start with a dot. The files will be created as `{prefix}{suffix}`.",
-      ignoreFocusOut: true,
+      title: "Files custom suffixes",
+      value: this.state.template?.suffixes.join(","),
     });
 
     if (!suffixesInput) {
@@ -165,53 +187,43 @@ export class CreateMultipleFilesCommand extends MultiStepForm<CreateMultipleFile
 
     const suffixes = suffixesInput
       .split(",")
-      .filter((item) => item === ",")
+      .filter((item) => item !== ",")
       .map((item) => item.trim());
 
     return suffixes;
   }
 
-  private async getDestinationFolderPrompt(
-    targetWorkspace: TWorkspaceFolder
-  ): Promise<Uri | undefined> {
-    const inputPath = await window.showInputBox({
-      title: "Destination folder",
+  private async getDestinationFolderPrompt(): Promise<
+    { uri: Uri; path: string } | undefined
+  > {
+    const inputPath = await this.showInputBox({
+      ignoreFocusOut: true,
+      placeHolder: "Enter relative folder path...",
       prompt:
         "Enter the relative path for the folder that will hold the new files (e.g., 'src/components'). If the folder path does not fully exists the needed folders will be created.\n",
-      placeHolder: "Enter relative folder path...",
-      ignoreFocusOut: true,
+      title: "Destination folder",
+      value: this.state.folder?.relativePath,
     });
 
-    return inputPath ? Uri.joinPath(targetWorkspace.uri, inputPath) : undefined;
+    return inputPath && this.state.workspace?.uri
+      ? {
+          uri: Uri.joinPath(this.state.workspace.uri, inputPath),
+          path: inputPath,
+        }
+      : undefined;
   }
 
   private async getFilesPrefixPrompt(): Promise<string | undefined> {
-    const prefix = await window.showInputBox({
-      title: "Files start name or prefix",
+    const prefix = await this.showInputBox({
+      ignoreFocusOut: true,
       placeHolder: "Enter the prefix...",
       prompt:
         "The files will be created as `{prefix}{suffix}`. The suffixes are the ones defined in your settings.json for the template you have selected or the ones you have specified for a custom template.",
-      ignoreFocusOut: true,
+      title: "Files start name or prefix",
+      value: this.state.prefix ?? undefined,
     });
 
     return prefix;
-  }
-
-  private async createFoldersAndFilesTask(
-    targetFolder: Uri,
-    suffixes: string[],
-    prefix: string
-  ): Promise<void> {
-    const destinationFolderUri = Uri.joinPath(targetFolder);
-
-    await this.createFolderStructure(destinationFolderUri);
-
-    for (const suffix of suffixes) {
-      const fileName = `${prefix}${suffix}`;
-      const fileUri = Uri.joinPath(destinationFolderUri, fileName);
-
-      await workspace.fs.writeFile(fileUri, new Uint8Array());
-    }
   }
 
   private async createFolderStructure(folderUri: Uri): Promise<void> {
--- src/utils/multi-step-input-form.ts
+++ src/utils/multi-step-input-form.ts
@@ -1,21 +1,53 @@
+import type { QuickInputButton, QuickPickItem } from "vscode";
+import { Disposable, QuickInputButtons, window } from "vscode";
+
 const INITIAL_STEP = 1;
+
+type QuickPickParameters<T> = {
+  activeItem?: T;
+  buttons?: QuickInputButton[];
+  ignoreFocusOut: boolean;
+  items: T[];
+  placeholder?: string;
+  title?: string;
+};
+
+type InputBoxParameters = {
+  buttons?: QuickInputButton[];
+  ignoreFocusOut: boolean;
+  prompt?: string;
+  title?: string;
+  value?: string;
+  placeholder?: string;
+};
+
+export type Step<FormState> = {
+  execute: () => Promise<void>;
+  shouldSkip?: (state: FormState) => boolean;
+  whenSkip?: () => void;
+};
+
+class ButtonAction {
+  static readonly Back = new ButtonAction();
+  static readonly Cancel = new ButtonAction();
+}
 
 export class MultiStepForm<FormState extends Record<string, unknown>> {
   /**
    * Steps are 1-indexed based;
    */
   private currentStep: number;
+  private skippedSteps: number;
   private totalSteps: number;
-  private readonly maxSteps: number;
 
   private _state: FormState;
 
-  constructor(maxSteps: number, initialState: FormState) {
+  constructor(initialState: FormState) {
     this._state = initialState;
 
+    this.skippedSteps = 0;
     this.currentStep = INITIAL_STEP;
-    this.totalSteps = maxSteps;
-    this.maxSteps = maxSteps;
+    this.totalSteps = INITIAL_STEP;
   }
 
   get state() {
@@ -34,24 +66,159 @@ export class MultiStepForm<FormState extends Record<string, unknown>> {
     this.state = newState;
   }
 
+  get displayStep() {
+    return this.currentStep - this.skippedSteps;
+  }
+
+  get displayTotalSteps() {
+    return this.totalSteps - this.skippedSteps;
+  }
+
   public previousStep() {
-    this.currentStep -= 1;
+    if (this.currentStep > INITIAL_STEP) {
+      this.currentStep -= 1;
+    }
   }
 
   public nextStep() {
     this.currentStep += 1;
   }
 
-  public skipOneStep() {
+  public skipStep() {
+    this.skippedSteps += 1;
     this.currentStep += 1;
-    this.totalSteps -= 1;
   }
 
-  public resetOneSkippedStep() {
-    this.totalSteps += 1;
+  public async stepThrough(stepHandlers: Record<number, Step<FormState>>) {
+    // Determine the initial total steps based on the step handlers provided
+    this.currentStep = INITIAL_STEP;
+    this.skippedSteps = 0;
+    this.totalSteps = Object.keys(stepHandlers).length;
+
+    let stop = false;
+
+    while (this.currentStep <= this.totalSteps || !stop) {
+      try {
+        const currentStepHandler = stepHandlers[this.currentStep];
+
+        // Check if this step should be skipped based on the current state
+        if (currentStepHandler.shouldSkip?.(this._state)) {
+          currentStepHandler.whenSkip?.();
+          this.skipStep();
+          continue;
+        }
+
+        await currentStepHandler.execute();
+
+        this.nextStep();
+      } catch (error) {
+        if (error === ButtonAction.Back && this.currentStep > INITIAL_STEP) {
+          this.previousStep();
+        } else {
+          break;
+        }
+      }
+    }
   }
 
-  public resetTotalSteps() {
-    this.totalSteps = this.maxSteps;
+  async showSingleSelectionQuickPick<
+    T extends QuickPickItem,
+    P extends QuickPickParameters<T>
+  >({
+    activeItem,
+    buttons,
+    ignoreFocusOut,
+    items,
+    placeholder,
+    title,
+  }: P): Promise<T | undefined> {
+    /** A Disposable array tracks resources (listeners and objects) created in this function so they can be disposed of properly, preventing memory leaks. */
+    const disposables: Disposable[] = [];
+
+    const quickPick = window.createQuickPick<T>();
+    quickPick.title = title;
+    quickPick.placeholder = placeholder;
+    quickPick.items = items;
+    quickPick.ignoreFocusOut = ignoreFocusOut;
+
+    if (activeItem) {
+      quickPick.activeItems = [activeItem];
+    }
+
+    quickPick.step = this.displayStep;
+    quickPick.totalSteps = this.displayTotalSteps;
+
+    quickPick.buttons = [
+      ...(this.displayStep > 1 ? [QuickInputButtons.Back] : []),
+      ...(buttons ?? []),
+    ];
+
+    return new Promise<T | undefined>((resolve, reject) => {
+      disposables.push(
+        quickPick.onDidTriggerButton((button) => {
+          if (button === QuickInputButtons.Back) {
+            reject(ButtonAction.Back);
+          }
+        }),
+
+        quickPick.onDidChangeSelection((items) => resolve(items[0])),
+
+        quickPick.onDidHide(() => reject(ButtonAction.Cancel))
+      );
+
+      quickPick.show();
+    }).finally(() => {
+      disposables.forEach((d) => d.dispose());
+      quickPick.hide();
+    });
+  }
+
+  async showInputBox<P extends InputBoxParameters>({
+    title,
+    value,
+    prompt,
+    buttons,
+    ignoreFocusOut,
+    placeholder,
+  }: P): Promise<string | undefined> {
+    /** A Disposable array tracks resources (listeners and objects) created in this function so they can be disposed of properly, preventing memory leaks. */
+    const disposables: Disposable[] = [];
+
+    const input = window.createInputBox();
+    input.title = title;
+    input.prompt = prompt;
+    input.ignoreFocusOut = ignoreFocusOut;
+    input.placeholder = placeholder;
+
+    if (value) {
+      input.value = value;
+    }
+
+    input.step = this.displayStep;
+    input.totalSteps = this.displayTotalSteps;
+
+    input.buttons = [
+      ...(this.displayStep > 1 ? [QuickInputButtons.Back] : []),
+      ...(buttons ?? []),
+    ];
+
+    return new Promise<string | undefined>((resolve, reject) => {
+      disposables.push(
+        input.onDidTriggerButton((button) => {
+          if (button === QuickInputButtons.Back) {
+            reject(ButtonAction.Back);
+          }
+        }),
+
+        input.onDidAccept(() => resolve(input.value)),
+
+        input.onDidHide(() => reject(ButtonAction.Cancel))
+      );
+
+      input.show();
+    }).finally(() => {
+      disposables.forEach((d) => d.dispose());
+      input.hide();
+    });
   }
 }
